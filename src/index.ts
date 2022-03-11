7794e3776353a3f8ea5ac3a6fb63213ec53503e2
Daehyun Kim <hatemogi@gmail.com>
2022-03-11 15:28:12 +0900

    큐브 표면에 6가지 색 입히기

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,29 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const materials = [
+  0xFF0000, 0x00FF00, 0x0000FF,
+  0xFFFA50, 0xFFFF00, 0xFFFFFF
+].map(color => new THREE.MeshToonMaterial({ color }));
+
+const cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
 
-camera.position.z = 5;
+const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+// const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
 
-renderer.render(scene, camera);
+camera.position.x = 2;
+camera.position.y = 1;
+camera.position.z = 7;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01; cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
