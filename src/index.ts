f08924aed70bef5b0d84a2215fcd1c055ca2a7d0
Daehyun Kim <hatemogi@gmail.com>
2022-03-11 14:41:17 +0900

    three.js로 박스 그려보기

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 const { Elm } = require('./Main.elm');
+import * as THREE from 'three';
 
 const mountNode = document.getElementById('elm-app');
 
 const app = Elm.Main.init({ node: mountNode });
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+renderer.render(scene, camera);
