f0c0d74a6dbdc1915f56d73e4f03029be6c4aa9a
Daehyun Kim <hatemogi@gmail.com>
2022-03-12 10:07:06 +0900

    큐브 색상 맞춰 추가

--- src/index.ts
+++ src/index.ts
@@ -13,27 +13,49 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const materials = [
-  0xFF0000, 0x00FF00, 0x0000FF,
-  0xFFFA50, 0xFFFF00, 0xFFFFFF
-].map(color => new THREE.MeshToonMaterial({ color }));
+function createCube(x: number, y: number, z: number): THREE.Mesh {
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  const [red, green, blue, orange, white, yellow] = [
+    0xFF0000, 0x00C000, 0x0000FF,
+    0xFF8C00, 0xFFFF00, 0xFFFFFF
+  ];
 
-const cube = new THREE.Mesh(geometry, materials);
-scene.add(cube);
+  const materials = [red, orange, blue, green, yellow, white]
+    .map(color => new THREE.MeshPhongMaterial({ color }));
 
-const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+  const cube = new THREE.Mesh(geometry, materials);
+  cube.position.add(new THREE.Vector3(x, y, z));
+  return cube;
+}
+
+const cubes = [
+  createCube(0, 0, 0),
+  createCube(3, 0, 0),
+  createCube(0, 3, 0),
+  createCube(3, 3, 0),
+  createCube(6, 3, 0),
+  createCube(6, 0, 0),
+  createCube(0, 6, 0),
+  createCube(3, 6, 0),
+  createCube(6, 6, 0),
+];
+
+cubes.forEach(cube => scene.add(cube));
+
+const light = new THREE.HemisphereLight(0xffffff, 0x080820, 1);
+light.position.set(-2, 3, 5);
 // const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
 
-camera.position.x = 2;
-camera.position.y = 1;
-camera.position.z = 7;
+camera.position.add(new THREE.Vector3(5, 5, 10));
 
-function animate() {
+function animate(time: number) {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01; cube.rotation.y += 0.01;
+  // time = 3400;
+  cubes.forEach(cube => {
+    cube.rotation.x = time * 0.0001; cube.rotation.y = time * 0.0001;
+  });
   renderer.render(scene, camera);
 }
 
-animate();
+requestAnimationFrame(animate);
