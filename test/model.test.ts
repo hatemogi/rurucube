d72e811f5ac911ea5613d43b102c8eb2ba827f34
Daehyun Kim <hatemogi@gmail.com>
2022-03-20 10:49:30 +0900

    Y이동 기본 테스트 추가

--- test/model.test.ts
+++ test/model.test.ts
@@ -31,10 +31,12 @@ test('move 4 times result in same cube', () => {
   const moveR4 = move(Move.R, Move.R, Move.R, Move.R);
   const moveL4 = move(Move.L, Move.L, Move.L, Move.L);
   const moveF4 = move(Move.F, Move.F, Move.F, Move.F);
+  const moveY4 = move(Move.Y, Move.Y, Move.Y, Move.Y);
   expectNoChange(moveU4);
   expectNoChange(moveR4);
   expectNoChange(moveL4);
   expectNoChange(moveF4);
+  expectNoChange(moveY4);
 });
 
 test('moving *_ after * results same cube', () => {
@@ -48,6 +50,7 @@ test('moving *_ after * results same cube', () => {
   expectNoChange(move(Move.F_, Move.F));
   expectNoChange(move(Move.D, Move.D_));
   expectNoChange(move(Move.D_, Move.D));
+  expectNoChange(move(Move.Y_, Move.Y));
 });
 
 test('moving R-pattern 6 times results same cube', () => {
