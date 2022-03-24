35e9d3e766a042edb86b822ab8df3e330a8d3b44
Daehyun Kim <hatemogi@gmail.com>
2022-03-24 15:40:44 +0900

    코드스타일 정돈

--- src/model.ts
+++ src/model.ts
@@ -9,12 +9,6 @@ function oppositeColor(color: FaceColor): FaceColor {
   return (color % 2) * -2 + color + 1;
 }
 
-enum FaceIndex {
-  DL, D, DR,
-  L, C, R,
-  UL, U, UR
-}
-
 type Cube = {
   front : Face,
   back  : Face,
@@ -54,8 +48,7 @@ type Move = {
 }
 
 const allMoves: Move[] = allSlices.flatMap(slice =>
-  [false, true].flatMap(prime =>
-    ({slice, prime})));
+  [false, true].flatMap(prime => ({slice, prime})));
 
 const eqFace = (f1: Face, f2: Face) =>
   f1.join() === f2.join();
@@ -69,6 +62,7 @@ const eqCube = (c1: Cube, c2: Cube) =>
   eqFace(c1.right, c2.right)
 
 type CubeFunc = (c: Cube) => Cube;
+type FaceFunc = (f: Face) => Face;
 
 function copyFace(from: Face, to: Face, toIndexes: number[]): Face {
   const result = to.concat([]);
@@ -76,7 +70,7 @@ function copyFace(from: Face, to: Face, toIndexes: number[]): Face {
   return result;
 }
 
-function reorderFace(from: (n: number) => number): ((face: Face) => Face) {
+function reorderFace(from: (n: number) => number): FaceFunc {
   return (face: Face) => {
     const target = face.concat([]);
     face.forEach((x, n) => target[from(n)] = x);
@@ -92,85 +86,77 @@ const clockwise = reorderFace(clockwiseF);
 const counterclockwise = reorderFace(counterclockwiseF);
 const halfTurn = reorderFace(halfTurnF);
 
-function curry<A, B, C>(f: (a: A, b: B) => C): (a: A) => (b: B) => C {
-  return (a: A) => (b: B) => f(a, b);
-}
+const moveU: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: copyFace(right, front, [6,7,8]),
+     back : copyFace(left,  back,  [6,7,8]),
+     up   : clockwise(up),
+     down,
+     left : copyFace(front, left,  [6,7,8]),
+     right: copyFace(back,  right, [6,7,8])
+  });
 
-type MoveFunc = (prime: boolean) => (cube: Cube) => Cube;
+const moveR: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: copyFace(down,           front, [2,5,8]),
+     back : copyFace(halfTurn(up),   back,  [0,3,6]),
+     up   : copyFace(front,          up,    [2,5,8]),
+     down : copyFace(halfTurn(back), down,  [2,5,8]),
+     left,
+     right: clockwise(right)
+  });
 
-const moveU: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: copyFace(right, front, [6,7,8]),
-  back : copyFace(left,  back,  [6,7,8]),
-  up   : clockwise(up),
-  down,
-  left : copyFace(front, left,  [6,7,8]),
-  right: copyFace(back,  right, [6,7,8])
-});
+const moveL: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: copyFace(up,             front, [0,3,6]),
+     back : copyFace(halfTurn(down), back,  [2,5,8]),
+     up   : copyFace(halfTurn(back), up,    [0,3,6]),
+     down : copyFace(front,          down,  [0,3,6]),
+     left : clockwise(left),
+     right
+  });
 
-const moveR: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: copyFace(down,           front, [2,5,8]),
-  back : copyFace(halfTurn(up),   back,  [0,3,6]),
-  up   : copyFace(front,          up,    [2,5,8]),
-  down : copyFace(halfTurn(back), down,  [2,5,8]),
-  left,
-  right: clockwise(right),
-});
+const moveF: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: clockwise(front),
+     back,
+     up   : copyFace(clockwise(left),  up,    [0,1,2]),
+     down : copyFace(clockwise(right), down,  [6,7,8]),
+     left : copyFace(clockwise(down),  left,  [2,5,8]),
+     right: copyFace(clockwise(up),    right, [0,3,6])
+  });
 
-const moveL: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: copyFace(up,             front, [0,3,6]),
-  back : copyFace(halfTurn(down), back,  [2,5,8]),
-  up   : copyFace(halfTurn(back), up,    [0,3,6]),
-  down : copyFace(front,          down,  [0,3,6]),
-  left : clockwise(left),
-  right,
-});
+const moveD: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: copyFace(left,  front, [0,1,2]),
+     back : copyFace(right, back,  [0,1,2]),
+     up,
+     down : clockwise(down),
+     left : copyFace(back,  left,  [0,1,2]),
+     right: copyFace(front, right, [0,1,2])
+  });
 
-const moveF: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: clockwise(front),
-  back,
-  up   : copyFace(clockwise(left),  up,    [0,1,2]),
-  down : copyFace(clockwise(right), down,  [6,7,8]),
-  left : copyFace(clockwise(down),  left,  [2,5,8]),
-  right: copyFace(clockwise(up),    right, [0,3,6]),
-});
+const moveB: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front,
+     back : clockwise(back),
+     up   : copyFace(counterclockwise(right), up,    [6,7,8]),
+     down : copyFace(counterclockwise(left),  down,  [0,1,2]),
+     left : copyFace(counterclockwise(up),    left,  [0,3,6]),
+     right: copyFace(counterclockwise(down),  right, [2,5,8])
+  });
 
-const moveD: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: copyFace(left,  front, [0,1,2]),
-  back : copyFace(right, back,  [0,1,2]),
-  up,
-  down : clockwise(down),
-  left : copyFace(back,  left,  [0,1,2]),
-  right: copyFace(front, right, [0,1,2]),
-});
+const moveX: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: down,
+     back : halfTurn(up),
+     up   : front,
+     down : halfTurn(back),
+     left : counterclockwise(left),
+     right: clockwise(right)
+  });
 
-const moveB: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front,
-  back : clockwise(back),
-  up   : copyFace(counterclockwise(right), up,    [6,7,8]),
-  down : copyFace(counterclockwise(left),  down,  [0,1,2]),
-  left : copyFace(counterclockwise(up),    left,  [0,3,6]),
-  right: copyFace(counterclockwise(down),  right, [2,5,8]),
-});
-
-function moveX({front, back, up, down, left, right}: Cube): Cube {
-  return {
-    front: down,
-    back : halfTurn(up),
-    up   : front,
-    down : halfTurn(back),
-    left : counterclockwise(left),
-    right: clockwise(right)
-  };
-}
-
-const moveY: CubeFunc = ({front, back, up, down, left, right}) => ({
-  front: right,
-  back : left,
-  up   : clockwise(up),
-  down : counterclockwise(down),
-  left : front,
-  right: back
-});
+const moveY: CubeFunc = ({front, back, up, down, left, right}) =>
+  ({ front: right,
+     back : left,
+     up   : clockwise(up),
+     down : counterclockwise(down),
+     left : front,
+     right: back
+  });
 
 function moveZ({front, back, up, down, left, right}: Cube): Cube {
   return {
@@ -183,12 +169,10 @@ function moveZ({front, back, up, down, left, right}: Cube): Cube {
   };
 }
 
-function repeat(count: number): ((f: CubeFunc) => CubeFunc) {
-  return (f: CubeFunc) => (c: Cube) => {
-    let result: Cube = c;
-    while (--count >= 0) result = f(result);
-    return result;
-  }
+const repeat = (count: number) => (f: CubeFunc) => (c: Cube) => {
+  let result: Cube = c;
+  while (--count >= 0) result = f(result);
+  return result;
 }
 
 const moveOne: (m: Move) => CubeFunc =
