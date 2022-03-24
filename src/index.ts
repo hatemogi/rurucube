dcb86c36aacc2a4277831fbd163e8c2f8de11ad4
Daehyun Kim <hatemogi@gmail.com>
2022-03-24 16:53:45 +0900

    뷰 소스 분리

--- src/index.ts
+++ src/index.ts
@@ -1,135 +1,7 @@
-import * as THREE from 'three';
-import * as Model from './model';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as M from './model';
+import * as V from './view';
 
-const mountNode = document.getElementById('elm-app');
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio( window.devicePixelRatio );
-document.body.appendChild(renderer.domElement);
-
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 500);
-
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.minDistance = 10;
-controls.maxDistance = 30;
-
-const CELL_SIZE = 2;
-const CELL_DISTANCE = CELL_SIZE * 1.03
-
-
-// https://colorswall.com/palette/171
-const [red, green, blue, orange, yellow, white, grey] = [
-  0xb71234, 0x009b48, 0x0046ad,
-  0xff5800, 0xffd500, 0xffffff, 0x222222
-].map(color => new THREE.MeshPhongMaterial({ color }));
-
-const materialTable: THREE.MeshPhongMaterial[] = (function() {
-  const table = [];
-  const C = Model.FaceColor;
-  table[C.WHITE]  = white;
-  table[C.YELLOW] = yellow;
-  table[C.BLUE]   = blue;
-  table[C.GREEN]  = green;
-  table[C.RED]    = red;
-  table[C.ORANGE] = orange;
-  table[C.NONE]   = grey;
-  return table;
-})();
-
-function cubeToMeshes({front, back, up, down, left, right}: Model.Cube): THREE.Mesh[] {
-  return [0, 1, 2].flatMap(z => [0, 1, 2].flatMap(y => [0, 1, 2].map(x => {
-    // z = 0 -> 앞쪽.
-    // z = 2 -> 뒷쪽
-
-    const geometry = new THREE.BoxGeometry(CELL_SIZE, CELL_SIZE, CELL_SIZE);
-
-    const grey = Model.FaceColor.NONE;
-    const M = materialTable;
-    const materials = [
-      // meterial 순서 => [right, left, up, down, front, back]
-      M[ x == 2 ? right[z + y*3    ] : grey ],
-      M[ x == 0 ? left [(2-z) + y*3] : grey ],
-      M[ y == 2 ? up   [x + z*3    ] : grey ],
-      M[ y == 0 ? down [x + (2-z)*3] : grey ],
-      M[ z == 0 ? front[x + y*3    ] : grey ],
-      M[ z == 2 ? back [(2-x) + y*3] : grey ]
-    ];
-
-    const mesh = new THREE.Mesh(geometry, materials);
-    mesh.position.set((x-1) * CELL_DISTANCE, (y-1) * CELL_DISTANCE, -(z-1) * CELL_DISTANCE);
-    return mesh;
-  })));
-}
-
-var cube = Model.defaultCube;
-var meshes = cubeToMeshes(cube);
-scene.add.apply(scene, meshes);
-
-const rotX: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationX(theta);
-const rotY: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationY(theta);
-const rotZ: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationZ(theta);
-
-type Layer = number[];
-const allMeshes = [0,1,2, 3,4,5, 6,7,8, 9,10,11, 12,13,14, 15,16,17, 18,19,20, 21,22,23, 24,25,26];
-
-const layers: Layer[] = [
-  [ 0, 1, 2,  3, 4, 5,  6, 7, 8],
-  [ 9,10,11, 12,13,14, 15,16,17],
-  [18,19,20, 21,22,23, 24,25,26],
-  allMeshes,
-
-  [ 0, 9,18,  3,12,21,  6,15,24],
-  [ 1,10,19,  4,13,22,  7,16,25],
-  [ 2,11,20,  5,14,23,  8,17,26],
-  allMeshes,
-
-  [ 6,15,24,  7,16,25,  8,17,26],
-  [ 3,12,21,  4,13,22,  5,14,23],
-  [ 0, 9,18,  1,10,19,  2,11,20],
-  allMeshes,
-];
-
-/*
-const light1 = new THREE.HemisphereLight(0xffffff, 0x080820, 1);
-light1.position.set(-5, 10, 15);
-
-const light2 = new THREE.HemisphereLight(0xffffff, 0x080820, 1);
-light2.position.set(-5, -10, 15);
-*/
-
-const light = new THREE.AmbientLight(0xFfFfFf); // white light
-scene.add(light);
-// scene.add(light1, light2);
-
-function setCameraPosition() {
-  camera.position.set(8, 8, 10);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-}
-
-
-function rotateLayer(layerIndex: number, theta: number) {
-  const matrix = (theta: number) => {
-    if (layerIndex < 4) return rotZ(theta);
-    else if (layerIndex < 8) return rotX(theta);
-    else return rotY(theta);
-  }
-  const m = matrix(theta);
-  layers[layerIndex].forEach(i => meshes[i].applyMatrix4(m));
-}
-
-function resetCubes(c: Model.Cube): Model.Cube {
-  scene.remove.apply(scene, meshes);
-  cube = c;
-  meshes = cubeToMeshes(cube);
-  scene.add.apply(scene, meshes);
-  return cube;
-}
-
-const commandHistory: Model.Move[] = [];
+const commandHistory: M.Move[] = [];
 
 type FrameTime = number;
 type AnimationCommand = {
@@ -142,8 +14,8 @@ type AnimationCommand = {
 
 const animationQueue: AnimationCommand[] = [];
 
-function moveToLayer({slice}: Model.Move): number {
-  const S = Model.Slice;
+function moveToLayer({slice}: M.Move): number {
+  const S = M.Slice;
   switch (slice) {
     case S.U: return 8;
     case S.D: return 10;
@@ -157,9 +29,9 @@ function moveToLayer({slice}: Model.Move): number {
   }
 }
 
-function moveToAngle({slice, prime}: Model.Move): number {
+function moveToAngle({slice, prime}: M.Move): number {
   const angle = Math.PI / 2;
-  const S = Model.Slice;
+  const S = M.Slice;
   switch (slice) {
     case S.U: case S.R: case S.F: case S.Y: case S.X: case S.Z:
       return prime ? angle : -angle;
@@ -168,7 +40,9 @@ function moveToAngle({slice, prime}: Model.Move): number {
   }
 }
 
-function move(m: Model.Move) {
+var cube: M.Cube = V.initCubes(M.defaultCube);
+
+function move(m: M.Move) {
   commandHistory.push(m);
   console.log("history", commandHistory);
   const layer = moveToLayer(m);
@@ -180,10 +54,10 @@ function move(m: Model.Move) {
     onTime: (t, animation) => {
       const dt = t - animation.processedUntil;
       const duration = animation.endAt - animation.startAt;
-      rotateLayer(layer, angle * (dt / duration));
+      V.rotateLayer(layer, angle * (dt / duration));
     },
     onFinish: t => {
-      resetCubes(Model.move(m)(cube));
+      cube = V.resetCubes(M.move(m)(cube));
     }
   });
 }
@@ -211,9 +85,9 @@ function doAnimation(t: FrameTime) {
 
 window.onkeydown = (ev: KeyboardEvent) => {
   console.log('keydown', ev.code, ev.shiftKey, ev.timeStamp);
-  const S = Model.Slice;
+  const S = M.Slice;
   const prime = ev.shiftKey;
-  const moveSlice = (slice: Model.Slice) => move({ slice, prime });
+  const moveSlice = (slice: M.Slice) => move({ slice, prime });
   switch (ev.code) {
     case "KeyU": moveSlice(S.U); break;
     case "KeyL": moveSlice(S.L); break;
@@ -224,19 +98,18 @@ window.onkeydown = (ev: KeyboardEvent) => {
     case "KeyX": moveSlice(S.X); break;
     case "KeyY": moveSlice(S.Y); break;
     case "KeyZ": moveSlice(S.Z); break;
-    case "Space": setCameraPosition(); break;
-    case "Escape": resetCubes(Model.defaultCube); break;
+    case "Space": V.setCameraPosition(); break;
+    case "Escape": cube = V.resetCubes(M.defaultCube); break;
   }
   return '';
 };
 
-setCameraPosition();
+V.setCameraPosition();
 
 function animate(time: number) {
   requestAnimationFrame(animate);
   doAnimation(time);
-  controls.update();
-  renderer.render(scene, camera);
+  V.render();
 }
 
 requestAnimationFrame(animate);
---
+++ src/view.ts
@@ -0,0 +1,140 @@
+import * as THREE from 'three';
+import * as M from './model';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 500);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 10;
+controls.maxDistance = 30;
+
+const CELL_SIZE = 2;
+const CELL_DISTANCE = CELL_SIZE * 1.03;
+
+/*
+const light1 = new THREE.HemisphereLight(0xffffff, 0x080820, 1);
+light1.position.set(-5, 10, 15);
+
+const light2 = new THREE.HemisphereLight(0xffffff, 0x080820, 1);
+light2.position.set(-5, -10, 15);
+*/
+
+const light = new THREE.AmbientLight(0xFfFfFf); // white light
+scene.add(light);
+// scene.add(light1, light2);
+
+function setCameraPosition() {
+  camera.position.set(8, 8, 10);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+// https://colorswall.com/palette/171
+const [red, green, blue, orange, yellow, white, grey] = [
+  0xb71234, 0x009b48, 0x0046ad,
+  0xff5800, 0xffd500, 0xffffff, 0x222222
+].map(color => new THREE.MeshBasicMaterial({ color }));
+
+const materialTable: THREE.MeshBasicMaterial[] = (function() {
+  const table = [];
+  const C = M.FaceColor;
+  table[C.WHITE]  = white;
+  table[C.YELLOW] = yellow;
+  table[C.BLUE]   = blue;
+  table[C.GREEN]  = green;
+  table[C.RED]    = red;
+  table[C.ORANGE] = orange;
+  table[C.NONE]   = grey;
+  return table;
+})();
+
+function cubeToMeshes({front, back, up, down, left, right}: M.Cube): THREE.Mesh[] {
+  return [0, 1, 2].flatMap(z => [0, 1, 2].flatMap(y => [0, 1, 2].map(x => {
+    // z = 0 -> 앞쪽.
+    // z = 2 -> 뒷쪽
+
+    const geometry = new THREE.BoxGeometry(CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+    const grey = M.FaceColor.NONE;
+    const T = materialTable;
+    const materials = [
+      // meterial 순서 => [right, left, up, down, front, back]
+      T[ x == 2 ? right[z + y*3    ] : grey ],
+      T[ x == 0 ? left [(2-z) + y*3] : grey ],
+      T[ y == 2 ? up   [x + z*3    ] : grey ],
+      T[ y == 0 ? down [x + (2-z)*3] : grey ],
+      T[ z == 0 ? front[x + y*3    ] : grey ],
+      T[ z == 2 ? back [(2-x) + y*3] : grey ]
+    ];
+
+    const mesh = new THREE.Mesh(geometry, materials);
+    mesh.position.set((x-1) * CELL_DISTANCE, (y-1) * CELL_DISTANCE, -(z-1) * CELL_DISTANCE);
+    return mesh;
+  })));
+}
+
+var cube = M.defaultCube;
+var meshes: THREE.Mesh[] = [];
+
+function resetCubes(c: M.Cube): M.Cube {
+  scene.remove.apply(scene, meshes);
+  meshes.forEach(m => m.geometry.dispose());
+  cube = c;
+  meshes = cubeToMeshes(cube);
+  scene.add.apply(scene, meshes);
+  return cube;
+}
+
+function initCubes(c: M.Cube): M.Cube {
+  meshes = cubeToMeshes(cube);
+  scene.add.apply(scene, meshes);
+  return cube;
+}
+
+const rotX: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationX(theta);
+const rotY: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationY(theta);
+const rotZ: (theta: number) => THREE.Matrix4 = theta => new THREE.Matrix4().makeRotationZ(theta);
+
+type Layer = number[];
+const allMeshes = [0,1,2, 3,4,5, 6,7,8, 9,10,11, 12,13,14, 15,16,17, 18,19,20, 21,22,23, 24,25,26];
+
+const layers: Layer[] = [
+  [ 0, 1, 2,  3, 4, 5,  6, 7, 8],
+  [ 9,10,11, 12,13,14, 15,16,17],
+  [18,19,20, 21,22,23, 24,25,26],
+  allMeshes,
+
+  [ 0, 9,18,  3,12,21,  6,15,24],
+  [ 1,10,19,  4,13,22,  7,16,25],
+  [ 2,11,20,  5,14,23,  8,17,26],
+  allMeshes,
+
+  [ 6,15,24,  7,16,25,  8,17,26],
+  [ 3,12,21,  4,13,22,  5,14,23],
+  [ 0, 9,18,  1,10,19,  2,11,20],
+  allMeshes,
+];
+
+function rotateLayer(layerIndex: number, theta: number) {
+  const matrix = (theta: number) => {
+    if (layerIndex < 4) return rotZ(theta);
+    else if (layerIndex < 8) return rotX(theta);
+    else return rotY(theta);
+  }
+  const m = matrix(theta);
+  layers[layerIndex].forEach(i => meshes[i].applyMatrix4(m));
+}
+
+function render() {
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+export { initCubes, resetCubes, rotateLayer, setCameraPosition, render };
